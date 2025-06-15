2f840085d376a6768775ce17ad9ee20473b1dcb4
pablohpsilva <pablohpsilva@proton.me>
2025-06-15 18:52:55 +0200

    testing

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -13,17 +13,17 @@ import { Stack } from "expo-router";
 import { StatusBar } from "expo-status-bar";
 import { LogBox } from "react-native";
 import "react-native-reanimated";
-// import PolyfillCrypto from "react-native-webview-crypto";
+import PolyfillCrypto from "react-native-webview-crypto";
 
 // import NDKHeadless from "@/components/NDKHeadless";
 import { NDKProvider } from "@/components/Context";
 import { DEFAULT_RELAYS } from "@/constants";
 import { DarkTheme, DefaultTheme } from "@/constants/Theme";
 import { useColorScheme } from "@/hooks/useColorScheme";
-import * as Sentry from '@sentry/react-native';
+import * as Sentry from "@sentry/react-native";
 
 Sentry.init({
-  dsn: 'https://8e0f3df32b2ebb086ed1e34cee8f4d72@o4509492843577344.ingest.de.sentry.io/4509492844691536',
+  dsn: "https://8e0f3df32b2ebb086ed1e34cee8f4d72@o4509492843577344.ingest.de.sentry.io/4509492844691536",
 
   // Adds more context data to events (IP address, cookies, user, etc.)
   // For more information, visit: https://docs.sentry.io/platforms/react-native/data-management/data-collected/
@@ -32,7 +32,10 @@ Sentry.init({
   // Configure Session Replay
   replaysSessionSampleRate: 0.1,
   replaysOnErrorSampleRate: 1,
-  integrations: [Sentry.mobileReplayIntegration(), Sentry.feedbackIntegration()],
+  integrations: [
+    Sentry.mobileReplayIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
 
   // uncomment the line below to enable Spotlight (https://spotlightjs.com)
   // spotlight: __DEV__,
@@ -81,7 +84,7 @@ export default Sentry.wrap(function RootLayout() {
   return (
     <NDKProvider relayUrls={Object.keys(DEFAULT_RELAYS)}>
       <ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
-        {/* <PolyfillCrypto /> */}
+        <PolyfillCrypto />
         {/* <NDKHeadless /> */}
         <Stack>
           <Stack.Screen name="features" options={{ headerShown: false }} />
